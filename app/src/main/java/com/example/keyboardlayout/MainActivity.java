9f0a3988c713e53454d510ad7b68c05a2de28065
Kentaro Ooka <kooka@eink.com>
2016-07-21 13:56:12 +0900

    レイアウトを下寄せに修正、メインから動的生成。

--- app/src/main/java/com/example/keyboardlayout/MainActivity.java
+++ app/src/main/java/com/example/keyboardlayout/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.keyboardlayout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RelativeLayout mainLayout = (RelativeLayout) findViewById(R.id.mainLayout);
+        mainLayout.removeAllViews();
+
+        getLayoutInflater().inflate(R.layout.keyboard_upper, mainLayout);     //  大文字キーボード
+//      getLayoutInflater().inflate(R.layout.keyboard_lower, mainLayout);       //  小文字キーボード
+//      getLayoutInflater().inflate(R.layout.keyboard_number, mainLayout);      //  英数記号キーボード
     }
 }
